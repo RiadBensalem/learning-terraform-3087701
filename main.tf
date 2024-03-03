e724cffe2b4d5ca92ece930d48dec19f59d52fc6
RiadBensalem <32767506+RiadBensalem@users.noreply.github.com>
2024-03-03 23:46:14 +0100

    load balancer

--- main.tf
+++ main.tf
@@ -57,3 +57,41 @@ resource "aws_instance" "blog" {
     Name = "HelloWorld"
   }
 }
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name            = "blog-alb"
+  load_balancer_type = "application"
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  # Security Group
+  security_groups = module.blog_sg.security_group_id
+
+
+  http_tcp_listeners = {
+    
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0 
+    
+  }
+
+  target_groups = [
+    name_prefix      = "blog-"
+    backend-protocol = "HTTP"
+    backend-port     = 80
+    target_type      = "instance"
+    targets = {
+      my_target = {
+        target_id = aws_instance.blog.id
+        port = 80
+      }
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
