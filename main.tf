d78c59cb7d6c714feba1d56822bde1d0650aefb8
RiadBensalem <32767506+RiadBensalem@users.noreply.github.com>
2024-03-04 00:05:09 +0100

    try fix 2

--- main.tf
+++ main.tf
@@ -60,12 +60,11 @@ resource "aws_instance" "blog" {
 
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
 
   name               = "blog-alb"
   load_balancer_type = "application"
   vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
+  subnets            = [module.blog_vpc.public_subnets]
   security_groups    = [module.blog_sg.security_group_id]
 
 
@@ -78,17 +77,13 @@ module "alb" {
     
     }
   }
-    
-      
-    
-  
 
-  target_groups = [
-    {
-      name_prefix      = "blog-"
-      backend-protocol = "HTTP"
-      backend-port     = 80
-      target_type      = "instance"
+  target_groups = {
+    ex-target = {
+      name_prefix = "blog-"
+      protocol    = "TCP"
+      port        = 80
+      target_type = "instance"
       targets = {
         my_target = {
           target_id = aws_instance.blog.id
@@ -96,7 +91,7 @@ module "alb" {
         }
       }
     }
-  ]
+  }
 
   tags = {
     Environment = "dev"
